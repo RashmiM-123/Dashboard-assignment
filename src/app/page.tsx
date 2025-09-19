e78a48706287218432f864883aa7482740bafef5
RashmiM-123 <ras02hmi@gmail.com>
2025-09-19 14:46:42 +0530

    remove image from main file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 
-import Image from "next/image";
 import { DashBoardContextProvider } from "./components/context/DashboardContext";
 import Dashboard from "./components/Dashboard";
-import AddWidget from "./components/AddWidget";
+
 
 export default function Home() {
   return (
